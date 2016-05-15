2ed95ac26602d455d3babcb5b9518cd35f2815cc
unicod3 <unicod3@hotmail.com>
2016-05-15 08:54:22 -0700

    Create SalesOrder Model

---
+++ WebApiExcercise/Models/SalesOrder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApiExcercise.Models
+{
+    public class SalesOrder
+    {
+        public int Id { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public virtual ICollection<SalesOrderLine> SalesOrderLine { get; set; }
+
+        public double TotalAmount { get; set; }
+    }
+
+    public class SalesOrderLine
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        [ForeignKey("Product")]
+        public int? ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}

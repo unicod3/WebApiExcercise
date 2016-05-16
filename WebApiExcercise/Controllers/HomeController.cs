580ac22d52c2c6f5c91d801ce4a779a30b8f57dd
unicod3 <unicod3@hotmail.com>
2016-05-15 17:34:33 -0700

    Create View pages for controllers

--- WebApiExcercise/Controllers/HomeController.cs
+++ WebApiExcercise/Controllers/HomeController.cs
@@ -15,9 +15,57 @@ namespace WebApiExcercise.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Supplier Page
+        /// </summary>
+        /// <returns>View</returns>
         public ActionResult Supplier()
         {
             ViewBag.Title = "Supplier Page";
+
+            return View();
+        }
+
+        /// <summary>
+        /// User Page
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult User()
+        {
+            ViewBag.Title = "User Page";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Product Page
+        /// </summary>
+        /// <returns>Product</returns>
+        public ActionResult Product()
+        {
+            ViewBag.Title = "Product Page";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Purchase Order Page
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult PurchaseOrder()
+        {
+            ViewBag.Title = "Purchase Order Page";
+
+            return View();
+        }
+
+        /// <summary>
+        /// SalesOrder Page
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult SalesOrder()
+        {
+            ViewBag.Title = "Sales Order Page";
 
             return View();
         }
